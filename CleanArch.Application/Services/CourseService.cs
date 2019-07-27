c552b009de635a3badecec1015e2993fd4b40f4c
Miqayel Avagyan <miqayel.avagyan@outlook.com>
2019-07-27 20:26:29 +0400

    initual commit DI or IoC

---
+++ CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Dtos;
+using CleanArch.Application.Interfaces;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+        public async Task<IEnumerable<CourseDto>> GetCourse()
+        {
+            var courses = await _courseRepository.GetCourses();
+
+            return null;
+        }
+    }
+}
---
+++ CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _context;
+        public CourseRepository(UniversityDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Course>> GetCourses()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+    }
+}
